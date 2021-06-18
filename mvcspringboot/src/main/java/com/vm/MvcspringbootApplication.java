053ad22538ebc1893940980c73c4cdad4eaacab4
Sai Teja <Sai Teja@LAPTOP-K7L883I0>
2021-06-18 13:02:27 +0530

    1st spring boot application

---
+++ mvcspringboot/src/main/java/com/vm/MvcspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcspringbootApplication.class, args);
+	}
+
+}
